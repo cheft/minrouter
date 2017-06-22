f263bd7ef146dcd969b481334b9bc9c421f4e5f9
ChenHaifeng <m@cheft.cn>
2017-06-22 13:58:40 +0800

    Add v1.0.0-pre code

--- README.md
+++ README.md
@@ -1,21 +1,9 @@
 # minrouter
-a micro client/server-side router, pushState, hashchange to support IE8+
+a micro client/server-side router [WIP]
 
-## Fatures
-* support html5 pushState
-* support hashchange if IE < 10
-* support isomorphic router (riot, react)
-* support hash-bang (#!), router.sep = '!'
-* support forced server-side url, router.sep = '/'
-
-## Install
+## test
     
-    npm install minrouter
+    npm install
+    npm run koa
+    npm run express
   
-or
-
-    git clone https://github.com/cheft/minrouter.git
-
-## Usage
-* see [example.html](https://github.com/cheft/minrouter/blob/master/example.html)
-* routes rules like backbone, [more](http://backbonejs.org/#Router-routes)
--- minrouter.js
+++ minrouter.js
@@ -1,117 +1,122 @@
-;(function() {
-    if(!window) return;
-    var win = window, supportPushState = 'pushState' in history, evt = supportPushState ? 'popstate' : 'hashchange', self = {};
+// koa
+// app.use(function(ctx, next) {
+// 	// ctx.req, ctx.res 是原生 node 的两个对象，下面两个是 koa 的
+//   console.log(ctx.request, ctx.response)
+// })
 
-    var regexps = [
-        /[\-{}\[\]+?.,\\\^$|#\s]/g,
-        /\((.*?)\)/g,
-        /(\(\?)?:\w+/g,
-        /\*\w+/g,
-    ],
-    getRegExp = function(route) {
-        route = route.replace(regexps[0], '\\$&')
-            .replace(regexps[1], '(?:$1)?')
-            .replace(regexps[2], function(match, optional) {
-                return optional ? match : '([^/?]+)'
-            }).replace(regexps[3], '([^?]*?)');
-        return new RegExp('^' + route + '(?:\\?([\\s\\S]*))?$');
-    },
-    extractParams = function(route, fragment) {
-        var params = route.exec(fragment).slice(1);
-        var results = [], i;
-        for(i = 0; i < params.length; i++) {
-            results.push(decodeURIComponent(params[i]) || null);
-        }
-        return results;
-    };
+// // express
+// app.use(function(req, res, next) {
+//   console.log(req, res, next)
+// })
 
-    function Router(opts) {
-        this.opts = opts;
-        this.routes = opts.routes;
-        this.sep = opts.sep || '';
-        this.emit();
-        this.holdLinks(opts.links || []);
-        self = this;
+/**
+* 暂时只支持 get 方式，负责页面渲染和跳转
+* koa 如果要做 api 服务器，还是用 koa-router
+* 两种参数：path 通配符的存入 req.params 中；问号传参的存入 req.query 中
+*/
+function MinRouter() {
+	return Router
+}
+
+var regexps = [
+    /[\-{}\[\]+?.,\\\^$|#\s]/g,
+    /\((.*?)\)/g,
+    /(\(\?)?:\w+/g,
+    /\*\w+/g,
+],
+extractRoute = function(route) {
+	var matchs = []
+  route = route.replace(regexps[0], '\\$&')
+    .replace(regexps[1], '(?:$1)?')
+    .replace(regexps[2], function(match, optional) {
+    	if (match) matchs.push(match.replace(':', ''))
+      return optional ? match : '([^/?]+)'
+    }).replace(regexps[3], function(match, optional) {
+    	if (match) matchs.push(match.replace('*', ''))
+    	return '([^?]*?)'
+    })
+  return {regexp: new RegExp('^' + route + '(?:\\?([\\s\\S]*))?$'), matchs: matchs}
+},
+
+extractParams = function(route, path) {
+	var params = route.exec(path).slice(1)
+	var results = [], i
+	for(i = 0; i < params.length; i++) {
+	  results.push(decodeURIComponent(params[i]) || null)
+	}
+	return results
+};
+
+function Router(req, res, next) {
+	if (next) { // express
+		this.req = req
+		this.res = res
+		this.next = next
+		this.env = 'express'
+	} else { // koa
+		this.ctx = req
+		this.req = this.ctx.request
+		this.res = this.ctx.response
+		this.next = res
+		this.env = 'koa'
+		console.log(this.req)
+	}
+	console.log(this.ctx.request)
+	if (typeof document === 'object') this.env = 'browser'
+
+	for(var i = 0; i < Router.routes.length; i++) {
+    var route = extractRoute(Router.routes[i].path);
+    // express: req.path, koa: req.url
+    var path = this.req.path || this.req.url
+    if (!route.regexp.test(path)) {
+        continue;
     }
-    Router.prototype.exec = function(path) {
-        for(var r in this.routes) {
-            var route = getRegExp(r);
-            if (!route.test(path)) {
-                continue;
-            }
-            if (typeof this.routes[r] === 'function') {
-                this.routes[r].apply(this, extractParams(route, path));
-            } else {
-                var fn = this.opts[this.routes[r]];
-                fn ? fn.apply(this, extractParams(route, path)) : void 0;
-            }
-        }
-    };
-    Router.prototype.emit = function(path) {
-        if(supportPushState) {
-            path = path.state.path;
-        }else {
-            path = location.href.split('#')[1] || '';
-        }
-        self.exec(path);
+    var results = extractParams(route.regexp, path)
+
+    this.req.params = this.req.params || {}
+
+    for (var j = 0; j < route.matchs.length; j++) {
+    	this.req.params[route.matchs[j]] = results[j]
     }
-    Router.prototype.start = function() {
-        win.addEventListener ? win.addEventListener(evt, this.emit, false) : win.attachEvent('on' + evt, this.emit)
-    };
-    Router.prototype.stop = function() {
-        win.removeEventListener ? win.removeEventListener(evt, this.emit, false) : win.detachEvent('on' + evt, this.emit);
-    };
-    Router.prototype.go = function(path, isReplace) {
-        if(supportPushState) {
-            if(isReplace) {
-                history.replaceState({path: path}, document.title, path);
-            }else {
-                history.pushState({path: path}, document.title, path);
-            }
-        }else {
-            if(this.sep !== '/') {
-                location.hash = this.sep + path;
-            }
-        }
-        this.exec(path);
-    };
-    Router.prototype.back = function() {
-        history.back();
-    };
-    Router.prototype.hold = function(e, href) {
-        var isReplace = false, path = href !== undefined ? href : e.target.pathname; 
-        if(!supportPushState) {
-            path = '/' + path;
-        }else {
-            if(path === history.state.path) {
-                isReplace = true;
-            }
-        }
-        this.go(path, isReplace);
-        if(e && e.preventDefault) {
-            e.preventDefault();      
-        }else {
-            e.returnValue = false;              
-            return false; 
-        }
-    };
-    Router.prototype.holdLinks = function(links) {
-        for(var i = 0; i < links.length; i++) {
-            links[i].onclick = function(e) {
-                var href;
-                if(!e) {
-                    e = win.event;
-                    href = this.pathname;
-                }
-                self.hold(e, href);
-            }
-        }
-    };
-    if(typeof exports === 'object') {
-        module.exports = Router;
-    }else if(typeof define === 'function' && define.amd) {
-        define(function() { return window.MinRouter = Router; })
-    }else {
-        window.MinRouter = Router;
+    if (this.env === 'express') {
+    	Router.routes[i].action.call(this, this.req, this.res, this.next)
+    } else if (this.env === 'koa') {
+    	Router.routes[i].action.call(this.ctx, this.ctx, this.next)
+    } else if (this.env === 'browser') {
+    	// :TODO
+    	Router.routes[i].action.call(this.ctx, this.ctx, this.next)
     }
-})(typeof window != 'undefined' ? window : undefined);
+	}
+}
+
+Router.routes = []
+
+Router.get = function(path, action) {
+	Router.routes.push({path: path, action: action})
+}
+
+function Request(req) {
+	this.params = req.params
+	this.query = req.query
+}
+
+function Response() {
+	
+}
+
+Response.prototype.render = function(view, data) {
+
+}
+
+Response.prototype.redirect = function(uri, isReplace) {
+	if (uri === 'back') {
+		return history.back()
+	}
+
+	if (isReplace) {
+		return history.replaceState({path: uri}, document.title, uri);
+	}
+	history.pushState({path: uri}, document.title, uri);
+}
+
+module.exports = MinRouter
